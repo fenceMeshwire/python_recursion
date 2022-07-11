b1861e93fe3415fd711c65e1491b54b6f4c242f7
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-07-11 21:26:54 +0200

    Create 01_basic_recursion.py
    Basic principle of recursion and the process of data storage and retrieval within the stack in Python3 (LIFO - Last In, First Out).

---
+++ 01_basic_recursion.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 01_basic_recursion.py
+
+# The stack is a data structure that is modeled according to the last-in-first-out (LIFO) principle.
+
+def use_recursive_function(int_number):
+    # first if-condition is to only to show the 'going-into-stack' process.
+    if int_number > 0: print(int_number, ' -> going into stack...')
+    # Base case / recursion scenario:
+    if int_number == 0:
+        print(int_number, ' Base case.')
+        return
+    else:
+        use_recursive_function(int_number - 1)
+        print(int_number, ' -> ...returning from stack')
+        return
+
+if __name__ == '__main__':
+    int_number = int(5)
+    use_recursive_function(int_number)
