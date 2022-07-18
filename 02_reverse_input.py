791e83581cfa2a7b7158f59f85c3f1db3c80cb00
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-07-18 13:10:23 +0200

    Update 02_reverse_input.py

--- 02_reverse_input.py
+++ 02_reverse_input.py
@@ -8,16 +8,17 @@ def reverse_input(given_string):
   
   # Last step, stopping condition:
   if len(given_string) == 0 or len(given_string) == 1:
-    return given_string
+    return given_string 
   
   # Repeating step, working condition:
   else:
     first = given_string[0]
     rest = given_string[1:]
-    # Values of variable "first" are stacking up reversed 
-    # with each repetition:
+    # Values of variable "first" are stacking up with each repetition:
     return reverse_input(rest) + first
 
 if __name__ == '__main__':
     print(reverse_input('1234567'))
+    # The content is returned according to the last-in-first-out (LIFO) principle.
     # result: '7654321'
+    
